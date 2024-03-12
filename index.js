2dfd435fa6d2a7a19eb9b0565c7db148ef48ac06
Fabian_PC <fb-224651@hs-weingarten.de>
2024-03-12 09:55:04 +0100

    Fix html und custom validation not being the same

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ email.addEventListener("input", () => {
   const localPartIsValid = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*$/;
   const hasAtSymbol = emailValue.includes("@");
   const domainPartIsValid = /^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
-
   if (!hasAtSymbol) {
     emailError.textContent = "missing @-symbol";
   } else {
     const [localPart, domainPart] = emailValue.split("@");
 
     if (!localPartIsValid.test(localPart)) {
+      email.setCustomValidity("local part (before @) is not valid!");
       emailError.textContent = "local part (before @) is not valid!";
     } else if (!domainPartIsValid.test(domainPart)) {
+      email.setCustomValidity("domain part (after @) is not valid!");
       emailError.textContent = "domain part (after @) is not valid!";
     } else {
+      email.setCustomValidity("");
       emailError.textContent = "";
     }
   }
